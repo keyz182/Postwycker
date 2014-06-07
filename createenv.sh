18bb3a8315681d4264ea936269e72db9f2a24e6f
Kieran Evans <keyz182@gmail.com>
2014-06-07 16:51:01 +0100

    Update createenv.sh

--- createenv.sh
+++ createenv.sh
@@ -8,7 +8,7 @@ source settings.conf
 PSQLCONTAINER=$(docker run --name $INSTANCE_NAME-postgres -d -P postgis21)
 echo $PSQLCONTAINER
 #COLLECTOR_CONTAINER=$(
-docker run --rm -d --name $INSTANCE_NAME-collector --link $INSTANCE_NAME-postgres:postgis21 \
+docker run -d --name $INSTANCE_NAME-collector --link $INSTANCE_NAME-postgres:postgis21 \
     -e COLLECTION_TYPE="$COLLECTION_TYPE" -e BBOX="$BBOX" -e SEARCHTERMS="$SEARCHTERMS" \
     -e TABLE_NAME="$TABLE_NAME" -e TWITTER_APP_KEY="$TWITTER_APP_KEY" \
     -e TWITTER_APP_SECRET="$TWITTER_APP_SECRET" -e TWITTER_OAUTH_TOKEN="$TWITTER_OAUTH_TOKEN" \
