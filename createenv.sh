c6c109793d740dfb385d3af92fb14fe22554e261
Kieran Evans <keyz182@gmail.com>
2014-06-07 18:04:51 +0100

    Update createenv.sh

--- createenv.sh
+++ createenv.sh
@@ -9,8 +9,9 @@ docker build -t postwycker .
 
 PSQLCONTAINER=$(docker run --name $INSTANCE_NAME-postgres -d -P keyz182/postgis21)
 echo $PSQLCONTAINER
+mkdir /tmp/logs
 #COLLECTOR_CONTAINER=$(
-docker run -d --name $INSTANCE_NAME-collector --link $INSTANCE_NAME-postgres:postgis21 \
+docker run -d --name $INSTANCE_NAME-collector -v /tmp/logs/:/var/log/supervisor/ --link $INSTANCE_NAME-postgres:postgis21 \
     -e COLLECTION_TYPE="$COLLECTION_TYPE" -e BBOX="$BBOX" -e SEARCHTERMS="$SEARCHTERMS" \
     -e TABLE_NAME="$TABLE_NAME" -e TWITTER_APP_KEY="$TWITTER_APP_KEY" \
     -e TWITTER_APP_SECRET="$TWITTER_APP_SECRET" -e TWITTER_OAUTH_TOKEN="$TWITTER_OAUTH_TOKEN" \
